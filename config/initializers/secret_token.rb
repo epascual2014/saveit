6801cca4ebab661dec7a553ee6113898c7cc5635
epascual2014 <Edrickpascual@yahoo.com>
2014-04-02 15:21:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saveit::Application.config.secret_token = 'aeb16935c3a3c9400064f566ded5a570d85d35b495986b005d4b543d7ed734d4e81be2dad541e505108aa27d02557ff4daaab933fe6a22af0917361f2106272e'
